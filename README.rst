51d3811bc639b53c16802a8fa554a5665f266241
Gaetan Semet <gaetan@xeberon.net>
2017-05-22 17:12:49 +0200

    do not use config yaml
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- README.rst
+++ README.rst
@@ -10,7 +10,7 @@ Installation:
 
 - clone this project to your Raspberry Pi
 - execute `./build.sh`
-- edit `config.yaml` according to your configuration
+- edit the python script according to your configuration
 - add this line to your crontab (use `crontab -e`):
 
     */5 * * * * /path/to/run.sh
--- refresh_domoticz.py
+++ refresh_domoticz.py
@@ -5,8 +5,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import yaml
-
 from pathlib import Path
 from time import sleep
 
@@ -16,13 +14,9 @@ from dhtxx import DHT22
 def read_dht22(pin):
     # Adjust pin (BCM) for your needs !
     dht22 = DHT22(pin)
-    return dht22.get_result_once()
+    return dht22.back()
 
 if __name__ == '__main__':
-    config_file = Path('config.yaml')
-    config_file.touch()
-    with config_file.open() as f:
-        config = yaml.load(f)
-    temp, humi = read_dht22(config.pin)
+    temp, humi = read_dht22(14)
     print(temp)
     print(humi)
