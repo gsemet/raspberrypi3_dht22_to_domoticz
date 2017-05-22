79b351cf012d6f655443990ea5fe7195cb5c9815
Gaetan Semet <gaetan@xeberon.net>
2017-05-22 22:48:57 +0200

    chdir
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 # Run this script as 'root' user (mandatory to access to GPIO)
 if [[ -z $VIRTUAL_ENV ]]; then
     . ./env/bin/activate
 fi
 
-cd $(dirname $0)
 python3 refresh_domoticz.py
